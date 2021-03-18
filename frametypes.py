56784c1e43a408b980bee74e4a3ec3a906b8c8a6
dekel241 <dekel241@gmail.com>
2021-03-18 02:12:02 +0200

    Add two new classes

---
+++ frametypes.py
@@ -0,0 +1,2 @@
+class FrameType:
+    Management = 0
---
+++ subtypes.py
@@ -0,0 +1,5 @@
+ # Subtypes of wireless client
+class ManagmentFrameSubType:
+    AssociationRequest = 0,
+    ReassociationRequest = 2,
+    ProbeRequest = 4, #Discover all available networks on specific channel
