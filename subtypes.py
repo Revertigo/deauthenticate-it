ffeefc0c245e6985784a98ffe1beb2479bf0d034
dekel241 <dekel241@gmail.com>
2021-03-19 00:26:47 +0200

    Fix bug of subtypes are tuples instead of ints

--- subtypes.py
+++ subtypes.py
@@ -1,5 +1,6 @@
  # Subtypes of wireless client
-class ManagmentFrameSubType:
-    AssociationRequest = 0,
-    ReassociationRequest = 2,
-    ProbeRequest = 4, #Discover all available networks on specific channel
+from enum import Enum
+class ManagmentFrameSubType(Enum):
+    AssociationRequest = 0
+    ReassociationRequest = 2
+    ProbeRequest = 4 #Discover all available networks on specific channel
