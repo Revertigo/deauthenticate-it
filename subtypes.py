9ed39e4c9d28beea83d5b7f7a493dd0601c77e46
dekel241 <dekel241@gmail.com>
2021-03-21 00:35:57 +0200

    Add new subtypes

--- subtypes.py
+++ subtypes.py
@@ -5,10 +5,13 @@ class ManagmentFrameSubType(IntEnum):
     ReassociationRequest = 2
     ProbeRequest = 4 #Discover all available networks on specific channel
     Authentication = 11
+    Action = 13
 
 class ControlFrameSubType(IntEnum):
     BAR = 8 #Block Ack Request
     BA = 9 #Block Ack
+    RTS = 11
 
 class DataFrameSubType(IntEnum):
+    QoS_Data = 8
     QOS_NULL = 12
